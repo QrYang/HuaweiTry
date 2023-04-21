957f0be82467e15c0d9d7174afa3803249cbe9a6
Kenora <kenora@163.com>
2023-04-21 17:35:21 +0800

    add calculator

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,4 +1,22 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class CalculatorTest {
-  
+    @Test
+    public void shouldReturn2When1Plus1() {
+        Calculator calculator = new Calculator();
+        int res = calculator.compute(1, 1, '+');
+        int resExp = 2;
+        Assert.assertEquals(resExp, res);
+    }
+
+    @Test
+    public void shouldReturn0When1Sub1() {
+        Calculator calculator = new Calculator();
+        int res = calculator.compute(1, 1, '-');
+        int resExp = 0;
+        Assert.assertEquals(resExp, res);
+    }
 }
